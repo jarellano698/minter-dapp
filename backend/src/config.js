260bdeb5061a8b259cabc3411b0ae47f5bd158ff
jarellano698 <jarellano698@gmail.com>
2022-04-10 13:06:29 -0600

    1

--- backend/src/config.js
+++ backend/src/config.js
@@ -62,16 +62,16 @@ const extraMetadata = {
 
 // ** REQUIRED **
 const AUTH = process.env.NFTPORT_API_KEY; // Set this in the .env file to prevent exposing your API key when pushing to Github
-const LIMIT = 4; // Your API key rate limit
+const LIMIT = 2; // Your API key rate limit
 const CHAIN = 'polygon'; // only rinkeby or polygon
 
 // REQUIRED CONTRACT DETAILS THAT CANNOT BE UPDATED LATER!
 const CONTRACT_NAME = 'Chunky Chicanos';
 const CONTRACT_SYMBOL = 'CC';
-const METADATA_UPDATABLE = true; // set to false if you don't want to allow metadata updates after minting
+const METADATA_UPDATABLE = false; // set to false if you don't want to allow metadata updates after minting
 const OWNER_ADDRESS = '0x6e5E483C9aBDC3E0D8aa94d707bD2FDa7aD13920';
 const TREASURY_ADDRESS = '0x6e5E483C9aBDC3E0D8aa94d707bD2FDa7aD13920';
-const MAX_SUPPLY = 10000; // The maximum number of NFTs that can be minted. CANNOT BE UPDATED!
+const MAX_SUPPLY = 10001; // The maximum number of NFTs that can be minted. CANNOT BE UPDATED!
 const MINT_PRICE = 15; // Minting price per NFT. Rinkeby = ETH, Polygon = MATIC. CANNOT BE UPDATED!
 const TOKENS_PER_MINT = 10; // maximum number of NFTs a user can mint in a single transaction. CANNOT BE UPDATED!
 
