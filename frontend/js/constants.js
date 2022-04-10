2b0fc23a4d4d71abfef28fac55fa65a275dafa5c
jarellano698 <jarellano698@gmail.com>
2022-04-10 13:30:20 -0600

    update contract address

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,5 +1,5 @@
-const contractAddress = "0x3FB27b34329f01485972D59691AD882517f702B2"; //Replace with your own contract address
-const chain = 'rinkeby'; // rinkeby or polygon
+const contractAddress = "0xA7Ce298105C12d24F82C8233218c0289542aC019"; //Replace with your own contract address
+const chain = 'polygon'; // rinkeby or polygon
 
 const welcome_h1 = "Welcome to the Chunky Chicanos project!!";
 const welcome_h2 = "Connect to MetaMask to Get Started";
